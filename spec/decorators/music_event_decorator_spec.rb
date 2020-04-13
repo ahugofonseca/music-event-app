3eb245cd543c2d85eda2d6802fc80a85bf1fe509
Hugo Fonseca <hugo.fonseca@brazilpetrostudies.com.br>
2020-04-13 17:55:41 -0300

    Add test to MusicEvent decorator class

---
+++ spec/decorators/music_event_decorator_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MusicEventDecorator do
+  let!(:music_event) do
+    FactoryBot.create(
+      :music_event,
+      scheduled_date_time: DateTime.parse('20/10/2020 01:30')
+    ).decorate
+  end
+  let!(:music_event_time_zone_converted) do
+    FactoryBot.create(
+      :music_event,
+      :with_time_zone_converted,
+      scheduled_date_time: DateTime.parse('20/10/2020 21:30')
+    ).decorate
+  end
+
+  it 'should music_event decorated' do
+    expect(music_event).to be_decorated
+  end
+
+  # Respond to validations
+  it { expect(music_event).to respond_to(:scheduled_date) }
+  it { expect(music_event).to respond_to(:scheduled_time) }
+
+  context 'when timezone was converted' do
+    it 'should return scheduled_date with converted Date UTC to localtime' do
+      music_event_sample = MusicEvent.all_in_time_zone('-0300').first.decorate
+      expect(music_event_sample.scheduled_date).to eq('10/19/2020')
+    end
+
+    it 'should return' do
+      music_event_sample = MusicEvent.all_in_time_zone('-0300').first.decorate
+      expect(music_event_sample.scheduled_time).to eq('22:30:00')
+    end
+  end
+
+  context 'when timezone default (UTC)' do
+    it 'should return the total count of transaction in integer' do
+      expect(music_event.scheduled_date).to eq('10/20/2020')
+    end
+
+    it 'should return the total count of transaction in integer' do
+      expect(music_event.scheduled_time).to eq('01:30:00')
+    end
+  end
+end
